5bfc4481cc4aa0a9d0cd10a2362df9583dad6c0a
BimoFr <bimofadhlurrahman@gmail.com>
2025-05-22 10:33:50 +0700

    Merapihkan codingan dan menambahkan logika traversal stack

--- file1.cpp
+++ file1.cpp
@@ -3,54 +3,65 @@
 using namespace std;
 
 // Node class representing a single node in the linked list
-class Node{
-public :
+class Node
+{
+public:
     int data;
-    Node* next;
+    Node *next;
 
-    Node(){
+    Node()
+    {
         next = NULL;
     }
 };
 
-//Stack class 
-class Stack {
-    private :
-    Node* top;  // Pointer to the top node of the stack
+// Stack class
+class Stack
+{
+private:
+    Node *top; // Pointer to the top node of the stack
 
-    public :
-    Stack(){
+public:
+    Stack()
+    {
         top = NULL; // Initialize the stack with a null top pointer
     }
 
-// Push operation : Insert an element onto the top of the stack
-int push(int value){
-    Node* newNode = new Node(); // 1. Allocate memory  for the new node 
-    newNode->data = value;  // 2. assign value
-    newNode->next = top;    // 3. Set the next pointer of the new node to the current top node
-    top = newNode;  // 4. Update the top pointer to the new node
-    cout << "Push value: " << value << endl; 
-    return value;
-}
-
-// Pop operation : Remove the topmost element from the stack
-void pop(){
-    if (isEmpty()){
-        cout << "Stack is empty." << endl;
+    // Push operation : Insert an element onto the top of the stack
+    int push(int value)
+    {
+        Node *newNode = new Node(); // 1. Allocate memory  for the new node
+        newNode->data = value;      // 2. assign value
+        newNode->next = top;        // 3. Set the next pointer of the new node to the current top node
+        top = newNode;              // 4. Update the top pointer to the new node
+        cout << "Push value: " << value << endl;
+        return value;
     }
 
-Node* temp = top;   // Create a temporary pointer to the top node
-top = top->next;    // Update the top pointer to the next node
-cout << "Popped value: " << top->data << endl;
-}
-
-// Peek/Top operation : Retrieve the value of the topmost element without removing it
-void peek()
+    // Pop operation : Remove the topmost element from the stack
+    void pop()
     {
-        if(top == NULL)
+        if (isEmpty())
+        {
+            cout << "Stack is empty." << endl;
+        }
+
+        Node *temp = top; // Create a temporary pointer to the top node
+        top = top->next;  // Update the top pointer to the next node
+        cout << "Popped value: " << top->data << endl;
+    }
+
+    // Peek/Top operation : Retrieve the value of the topmost element without removing it
+    void peek()
+    {
+        if (top == NULL)
         {
             cout << "List is empty." << endl;
         }
+        else
+        {
+            Node *current = top;
+        } 
     }
 
 };
