685d57a1221ffcceaa5c2f33cb55cd2fce6ada31
BimoFr <bimofadhlurrahman@gmail.com>
2025-05-22 10:34:49 +0700

    Menambahkan logika perulangan

--- file1.cpp
+++ file1.cpp
@@ -61,6 +61,12 @@ public:
         else
         {
             Node *current = top;
+            while (current != NULL)
+            {
+                cout << current->data << " " << endl;
+                current = current->next;
+            }
+            cout << endl;
         } 
     }
 
