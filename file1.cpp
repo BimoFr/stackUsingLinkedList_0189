4a70e782f4318dab68db3863e7bc403c7056b7d8
BimoFr <bimofadhlurrahman@gmail.com>
2025-05-22 10:09:33 +0700

    Menambahkan pengecekan stack kosong pada fungsi pop

--- file1.cpp
+++ file1.cpp
@@ -32,4 +32,11 @@ int push(int value){
     cout << "Push value: " << value << endl; 
     return value;
 }
+
+// Pop operation : Remove the topmost element from the stack
+void pop(){
+    if (isEmpty()){
+        cout << "Stack is empty." << endl;
+    }
+}
 };
